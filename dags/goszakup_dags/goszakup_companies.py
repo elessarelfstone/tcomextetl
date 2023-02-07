c6fd227fe86e9d8a7742f41ae11d59efbd196e7e
elessarelfstone <assalamdostlar@gmail.com>
2023-02-07 14:11:50 +0500

    test fixes

--- dags/goszakup_dags/goszakup_companies.py
+++ dags/goszakup_dags/goszakup_companies.py
@@ -15,7 +15,7 @@ with DAG(
         dag_id='goszakup_companies',
         catchup=False,
         start_date=pendulum.datetime(2023, 2, 1, tz=f'{Variable.get("TZ")}'),
-        schedule_interval='2 0 * * *',
+        schedule_interval='@daily',
         tags=['goszakup']
      ) as dag:
 
