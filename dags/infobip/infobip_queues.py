68bf0f3c887fbbbe6c0c4dc1f311347d5788f22a
elessarelfstone <assalamdostlar@gmail.com>
2023-05-06 07:40:31 +0500

    add infobip queues DAG

---
+++ dags/infobip/infobip_queues.py
@@ -0,0 +1,39 @@
+import sys
+from datetime import datetime, timedelta
+
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.models import DAG
+from airflow.models import Variable
+
+sys.path.append('.')
+
+from dags.docker_runner import ExternalEtlDockerRunner as Runner
+from dags.goszakup_dags.goszakup_common import prepare_command_args
+
+with DAG(
+        dag_id='infobip_queues',
+        catchup=False,
+        start_date=pendulum.datetime(2023, 2, 1, tz=f'{Variable.get("TZ")}'),
+        schedule_interval='@daily',
+        tags=['infobip']
+     ) as dag:
+
+    command_args = PythonOperator(
+        task_id='command_args',
+        python_callable=prepare_command_args,
+        dag=dag,
+        do_xcom_push=False
+    )
+
+    infobip_queues = Runner(
+        task_id='infobip_queues',
+        luigi_module='infobip',
+        luigi_task='InfobipQueues',
+        env_vars={'INFOBIP_USER': Variable.get('INFOBIP_USER'),
+                  'INFOBIP_PASSWORD': Variable.get('INFOBIP_PASSWORD')},
+        pool='infobip',
+        do_xcom_push=False
+    )
+
+    command_args >> infobip_queues
