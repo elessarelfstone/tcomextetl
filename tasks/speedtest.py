1c210fe4dfd31d9511fcc0201bc07363cefac9bb
elessarelfstone <elessarelfstone@mail.ru>
2023-06-29 07:42:04 +0500

    src:fix:speedtest refactor

--- tasks/speedtest.py
+++ tasks/speedtest.py
@@ -1,6 +1,5 @@
 import csv
 import json
-from datetime import datetime, timedelta
 from math import floor
 from time import sleep
 
@@ -11,8 +10,7 @@ from luigi.util import requires
 from tasks.base import CsvFileOutput, FtpUploadedOutput, Runner
 from tcomextetl.common.arch import extract_by_wildcard
 from tcomextetl.common.csv import CSV_QUOTECHAR
-from tcomextetl.common.dates import DEFAULT_FORMAT, today, n_days_ago
-from tcomextetl.common.exceptions import ExternalSourceError
+from tcomextetl.common.dates import DEFAULT_FORMAT, n_days_ago
 from tcomextetl.common.utils import build_fpath, append_file
 from tcomextetl.extract.speedtest_requests import SpeedTestDownloader
 from settings import TEMP_PATH, SPEEDTEST_USER, SPEEDTEST_PASS
@@ -34,11 +32,37 @@ class SpeedTestOutput(CsvFileOutput):
             'password': self.password
         }
 
-    def _merge_csvs(self, files) -> tuple[int, int]:
+    def run(self):
+
+        wildcard = '*.csv'
+        # separator of speedtest csv files
+        d = SpeedTestDownloader(
+            api_url,
+            *self.from_to,
+            self.dataset,
+            auth=self.auth
+        )
+        f_paths = []
+        d_count = 0
+
+        # download and unpacking
+        for file in d:
+            af_path = build_fpath(TEMP_PATH, self.name, '.zip')
+            d.download(af_path, file['url'])
+            f_path, *_ = extract_by_wildcard(af_path, wildcard=wildcard)
+            f_paths.append(f_path)
+            d_count += 1
+            self.set_status_info(
+                f'File: {file["name"]} .Downloading and unpacking...',
+                floor((d_count * 100) / d.length)
+            )
+            sleep(5)
+
         df = pd.DataFrame()
         row_count = 0
         file_count = 0
-        for count, file in enumerate(files):
+        # merging csv files
+        for count, file in enumerate(f_paths):
             data = pd.read_csv(file, sep=',')
             row_count += len(data.index) - 1
             file_count += 1
@@ -46,7 +70,7 @@ class SpeedTestOutput(CsvFileOutput):
 
             self.set_status_info(
                 f'Merging file {file}.',
-                floor((count * 100) / len(files))
+                floor((count * 100) / len(f_paths))
             )
 
         df.to_csv(
@@ -56,67 +80,6 @@ class SpeedTestOutput(CsvFileOutput):
             quoting=csv.QUOTE_ALL,
             quotechar=CSV_QUOTECHAR
         )
-
-        return row_count, file_count
-
-    def run(self):
-
-        wildcard = '*.csv'
-        # separator of speedtest csv files
-        sep = ','
-        d = SpeedTestDownloader(api_url, self.from_to, self.dataset, auth=self.auth)
-        datafiles = d.datafiles()
-        datafiles.reverse()
-        # print(datafiles)
-        row_count = 0
-        file_count = 0
-        # for a one date
-        if self.from_to[0] == self.from_to[1]:
-            file = list(filter(lambda df: df['date'] == self.from_to[0], datafiles))
-            if file:
-                af_path = build_fpath(TEMP_PATH, self.name, '.zip')
-                d.download(af_path, file.pop()['url'])
-                f_path = extract_by_wildcard(af_path, wildcard=wildcard)
-                buffer = pd.read_csv(f_path[0], sep=sep)
-                buffer.to_csv(
-                    self.output_fpath,
-                    header=False,
-                    sep=self.sep,
-                    quoting=csv.QUOTE_ALL,
-                    quotechar=CSV_QUOTECHAR
-                )
-                row_count = len(buffer.index)
-                file_count = 1
-            else:
-                raise ExternalSourceError(f'No data file for date - {self.from_to[0]}')
-
-        else:
-            s_date = datetime.strptime(self.from_to[0], DEFAULT_FORMAT)
-            e_date = datetime.strptime(self.from_to[1], DEFAULT_FORMAT)
-            dates_range = pd.date_range(s_date, e_date, freq='d').strftime('%Y-%m-%d').tolist()
-
-            # check dates
-            for dt in dates_range:
-                if dt not in [df['date'] for df in datafiles]:
-                    raise ExternalSourceError(f'No data file for date - {dt}')
-
-            f_paths = []
-            parsed_count = 0
-            for dt in dates_range:
-                file = list(filter(lambda df: df['date'] == dt, datafiles))
-                af_path = build_fpath(TEMP_PATH, self.name, '.zip')
-                file = file.pop()
-                d.download(af_path, file['url'])
-                f_path, *_ = extract_by_wildcard(af_path, wildcard=wildcard)
-                f_paths.append(f_path)
-                parsed_count += 1
-                self.set_status_info(
-                    f'File: {file["name"]} .Downloading and unpacking...',
-                    floor((parsed_count * 100) / len(dates_range))
-                    )
-                sleep(5)
-
-            row_count, file_count = self._merge_csvs(f_paths)
 
         stat = {'file_count': file_count, 'row_count': row_count}
         append_file(self.success_fpath, json.dumps(stat))
@@ -158,4 +121,3 @@ class SpeedtestFixedNetworkPerformance(SpeedTestRunner):
 
 if __name__ == '__main__':
     luigi.run()
-
--- tcomextetl/extract/speedtest_requests.py
+++ tcomextetl/extract/speedtest_requests.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 
+import pandas as pd
 
-from tcomextetl.extract.http_requests import Downloader, HttpRequest
+from tcomextetl.common.dates import DEFAULT_FORMAT
+from tcomextetl.common.exceptions import ExternalSourceError
+from tcomextetl.extract.http_requests import Downloader
 
 
 class SpeedTestDownloader(Downloader):
@@ -8,24 +12,50 @@ class SpeedTestDownloader(Downloader):
     def __init__(
         self,
         url,
-        period: tuple,
+        start: str,
+        end: str,
         dataset: str,
         params=None,
         headers=None,
         auth=None,
         timeout=None
     ):
-        super().__init__(url, params=params, headers=headers, auth=auth, timeout=timeout)
+        super().__init__(
+            url,
+            params=params,
+            headers=headers,
+            auth=auth,
+            timeout=timeout
+        )
         self.dataset = dataset
-        # self.url = url
-        self.period = period
 
-    def datafiles(self) -> list[dict]:
+        all_files = self._all_files()
+
+        dates_range = pd.date_range(
+            datetime.strptime(start, DEFAULT_FORMAT),
+            datetime.strptime(end, DEFAULT_FORMAT),
+            freq='d'
+        ).strftime('%Y-%m-%d').tolist()
+
+        self._datafiles = []
+
+        # filter dates
+        for dt in dates_range:
+            if dt not in [df['date'] for df in all_files]:
+                raise ExternalSourceError(f'No data file for date - {dt}')
+            else:
+                self._datafiles.append(
+                    list(filter(lambda df: df['date'] == dt, all_files)).pop()
+                )
+
+    def _all_files(self) -> list[dict]:
+
         files = []
         root = self.request(self.url).json()
 
         def _files(items):
             for item in items:
+                # parse tree-like structure
                 if item['type'] == 'file' and item['name'].find('headers') == -1 and '_20' in item['name']:
                     dataset = item['name'][:item['name'].index('_20')]
                     date = str(item['name']).split('.')[0].split('_')[1]
@@ -34,7 +64,6 @@ class SpeedTestDownloader(Downloader):
                         files.append({
                             'dataset': dataset,
                             'name': item['name'],
-                            # 'date': datetime.strptime(date, DEFAULT_FORMAT),
                             'date': date,
                             'url': item['url'],
                             'age': item['mtime']
@@ -43,5 +72,14 @@ class SpeedTestDownloader(Downloader):
                     sub_items = self.request(f'{self.url}{item["url"]}').json()
                     _files(sub_items)
 
+        # recursive call
         _files(root)
         return files
+
+    @property
+    def length(self):
+        return len(self._datafiles)
+
+    def __iter__(self):
+        for file in self._datafiles:
+            yield file
