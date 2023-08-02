f6a0c1d447bbd45c363f4547a2e9e49ece855189
elessarelfstone <elessarelfstone@mail.ru>
2023-08-02 17:54:48 +0500

    src:telecomkz: add conversions dags

---
+++ dags/telecomkz_dags/telecomkz_conversions_main.py
@@ -0,0 +1,41 @@
+import sys
+
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.models import DAG
+from airflow.models import Variable
+
+sys.path.append('.')
+
+from dags.docker_runner import ExternalEtlDockerRunner as Runner
+from dags.common import get_command_args
+
+with DAG(
+        dag_id='telecomkz_conversions_main',
+        catchup=False,
+        start_date=pendulum.now(tz=f'{Variable.get("TZ")}').subtract(days=1),
+        schedule_interval='@daily',
+        tags=['telecomkz']
+     ) as dag:
+
+    command_args = PythonOperator(
+        task_id='command_args',
+        python_callable=get_command_args,
+        dag=dag,
+        do_xcom_push=False,
+        op_kwargs={
+            'n_days_delta': 1
+        }
+    )
+
+    telecomkz_conversions_main = Runner(
+        task_id='telecomkz_conversions_main',
+        luigi_module='telecomkz',
+        luigi_task='TelecomkzYandexConversionsMetricaRepsMain',
+        luigi_params="{{ task_instance.xcom_pull(task_ids='command_args', key='command_args') }}",
+        env_vars={'TELECOMKZ_YANDEX_METRICA_TOKEN': Variable.get('TELECOMKZ_YANDEX_METRICA_TOKEN')},
+        pool='telecomkz',
+        do_xcom_push=False
+    )
+
+    command_args >> telecomkz_conversions_main
