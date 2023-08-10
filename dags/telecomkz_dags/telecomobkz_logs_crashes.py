1cc8eeed24a100a4f558179419e715bbb4c7906e
elessarelfstone <elessarelfstone@mail.ru>
2023-08-10 16:17:23 +0500

    dags:add: logs crashes

---
+++ dags/telecomkz_dags/telecomobkz_logs_crashes.py
@@ -0,0 +1,41 @@
+import sys
+
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.models import DAG
+from airflow.models import Variable
+
+sys.path.append('.')
+
+from dags.docker_runner import ExternalEtlDockerRunner as Runner
+from dags.common import get_command_args
+
+with DAG(
+        dag_id='telecomobkz_logs_crashes',
+        catchup=False,
+        start_date=pendulum.now(tz=f'{Variable.get("TZ")}').subtract(days=1),
+        schedule_interval='0 1 * * *',
+        tags=['telecomobkz']
+     ) as dag:
+
+    command_args = PythonOperator(
+        task_id='command_args',
+        python_callable=get_command_args,
+        dag=dag,
+        do_xcom_push=False,
+        op_kwargs={
+            'n_days_delta': 1
+        }
+    )
+
+    telecomobkz_logs_crashes = Runner(
+        task_id='telecomobkz_logs_crashes',
+        luigi_module='telecomkz',
+        luigi_task='TelecomobkzYandexMetricaLogsCrushes',
+        luigi_params="{{ task_instance.xcom_pull(task_ids='command_args', key='command_args') }}",
+        env_vars={'TELECOMOBKZ_YANDEX_APP_METRICA_TOKEN': Variable.get('TELECOMOBKZ_YANDEX_APP_METRICA_TOKEN')},
+        pool='telecomobkz',
+        do_xcom_push=False
+    )
+
+    command_args >> telecomobkz_logs_crashes
