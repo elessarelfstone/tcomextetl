23693f261c691a671950407798f4f32b88713290
kazhe <kazhenov.as1@gmail.com>
2024-01-19 09:50:18 +0600

    Add data_date for aitu_notification task

--- tasks/aitu.py
+++ tasks/aitu.py
@@ -4,7 +4,6 @@ from math import floor
 from datetime import datetime, timedelta
 from time import sleep
 
-
 import luigi
 import pandas as pd
 from luigi.parameter import ParameterVisibility
@@ -16,12 +15,13 @@ from tcomextetl.common.csv import save_csvrows, dict_to_row, CSV_QUOTECHAR
 from tcomextetl.common.dates import DEFAULT_FORMAT, n_days_ago
 from tcomextetl.common.utils import build_fpath, append_file, rewrite_file
 from tcomextetl.extract.aitu_requests import AituRequests, AituNotificationRequests
-from settings import AITU_API_KEY, AITU_SECRET_KEY, AITU_PUSH_NOTIFICATIONS_PROJECT_ID, AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY_ID, AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY, AITU_PUSH_NOTIFICATIONS_CLIENT_ID
+from settings import AITU_API_KEY, AITU_SECRET_KEY, AITU_PUSH_NOTIFICATIONS_PROJECT_ID, \
+    AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY_ID, AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY, AITU_PUSH_NOTIFICATIONS_CLIENT_ID
 
 api_url = f"https://amplitude.com/api/2/export"
 amplitude_id = 505199
 
-scopes=["https://www.googleapis.com/auth/cloud-platform"]
+scopes = ["https://www.googleapis.com/auth/cloud-platform"]
 creds_dict = {
     "type": "service_account",
     "client_email": "kt-411@asiachat-8dd78.iam.gserviceaccount.com",
@@ -31,9 +31,9 @@ creds_dict = {
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/kt-411%40asiachat-8dd78.iam.gserviceaccount.com",
     "universe_domain": "googleapis.com"
 }
-query = """
-    SELECT * FROM `asiachat-8dd78.telecom.push_raw_data`
-"""
+query = """SELECT * FROM `asiachat-8dd78.telecom.push_raw_data`"""
+query_date = """WHERE TIMESTAMP_TRUNC(server_time, DAY) = TIMESTAMP("{}")"""
+
 
 class AituOutput(CsvFileOutput):
     from_to = luigi.TupleParameter(default=())
@@ -48,7 +48,6 @@ class AituOutput(CsvFileOutput):
         }
 
     def run(self):
-
         params = {
             "start": self.from_to[0],
             "end": self.from_to[1]
@@ -62,7 +61,6 @@ class AituOutput(CsvFileOutput):
         )
 
         for d in parser:
-
             save_csvrows(self.output_fpath,
                          [dict_to_row(_d, self.struct, '$') for _d in d],
                          quotechar='"')
@@ -100,15 +98,26 @@ class AituRunner(Runner):
 
 
 class AituLogs(AituRunner):
-
     name = luigi.Parameter('aitu_logs')
 
 
 class AituNotificationOutput(CsvFileOutput):
-    project_id = luigi.TupleParameter(default=AITU_PUSH_NOTIFICATIONS_PROJECT_ID, visibility=ParameterVisibility.HIDDEN)
-    private_key_id = luigi.Parameter(default=AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY_ID, visibility=ParameterVisibility.HIDDEN)
+    project_id = luigi.Parameter(default=AITU_PUSH_NOTIFICATIONS_PROJECT_ID, visibility=ParameterVisibility.HIDDEN)
+    private_key_id = luigi.Parameter(default=AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY_ID,
+                                     visibility=ParameterVisibility.HIDDEN)
     private_key = luigi.Parameter(default=AITU_PUSH_NOTIFICATIONS_PRIVATE_KEY, visibility=ParameterVisibility.HIDDEN)
     client_id = luigi.Parameter(default=AITU_PUSH_NOTIFICATIONS_CLIENT_ID, visibility=ParameterVisibility.HIDDEN)
+    data_date = luigi.Parameter(default='')
+
+    @property
+    def request_sql(self):
+        sql = query
+
+        if self.data_date:
+            query_format = query_date.format(self.data_date)
+            sql = f"{query} {query_format}"
+
+        return sql
 
     def run(self):
         creds_dict['project_id'] = self.project_id
@@ -116,7 +125,8 @@ class AituNotificationOutput(CsvFileOutput):
         creds_dict['private_key'] = self.private_key
         creds_dict['client_id'] = self.client_id
 
-        parser = AituNotificationRequests(creds_dict, scopes, self.project_id, query)
+        parser = AituNotificationRequests(creds_dict, scopes, self.project_id, self.request_sql)
+
         df_aitu = parser.load()
         rows_count = 0
 
@@ -137,10 +147,17 @@ class AituNotificationOutputFtpOutput(FtpUploadedOutput):
 
 
 class AituNotificationRunner(Runner):
+    name = luigi.Parameter()
+    start_date = luigi.DateParameter(default=n_days_ago())
+    end_date = luigi.DateParameter(default=n_days_ago())
 
     @property
     def params(self):
         params = super(AituNotificationRunner, self).params
+
+        if not self.all_data:
+            params['data_date'] = self.start_date.strftime(DEFAULT_FORMAT)
+
         return params
 
     def requires(self):
